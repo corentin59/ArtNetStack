afb72b7bdbad8d805365f114489623db6463ff90
Azelart Corentin <corentin.azelart@capgemini.com>
2012-07-30 15:55:56 +0200

    Check-Style pass

--- src/main/java/fr/azelart/artnetstack/utils/ArtNetPacketDecoder.java
+++ src/main/java/fr/azelart/artnetstack/utils/ArtNetPacketDecoder.java
@@ -36,43 +36,52 @@ import fr.azelart.artnetstack.domain.enums.UniverseAddressProgrammingAuthorityEn
  */
 public class ArtNetPacketDecoder {
 
-	public static ArtNetObject decodeArtNetPacket( byte[] packet ) {
+	public static ArtNetObject decodeArtNetPacket(byte[] packet) {
 		
 		// The ArtNetPacket.
 		ArtNetObject artNetObject = null;
 		
 		// Set generals infos
-		final String hexaBrut = byteArrayToHex( packet );
-		final String id = new String (packet, 0, 7 );
+		final String hexaBrut = byteArrayToHex(packet);
+		final String id = new String(packet, 0, 7);
 		final String opCode = hexaBrut.substring(16, 20);
-		
+
 		// Yes, it's a ArtNetPacket
-		if ( !"Art-Net".equals( id ) ) {
-			return null; 
+		if (!"Art-Net".equals(id)) {
+			return null;
 		}
 
 		/*
 		 * Dicover the type of the packet.
 		 * Please refer to OpcodeTable.
 		 */
-		if ( OpCodeConstants.OPPOLL.equals( opCode ) ) {
-			// ArtPollPacket : This is an ArtPoll packet, no other data is contained in this UDP packet
-			if ( !checkVersion( packet, hexaBrut ) )
+		if (OpCodeConstants.OPPOLL.equals(opCode)) {
+			/*
+			 * ArtPollPacket : This is an ArtPoll packet,
+			 * no other data is contained in this UDP packet
+			 */
+			if (!checkVersion(packet, hexaBrut)) {
 				return null;
-			return decodeArtPollPacket( packet, hexaBrut );
-		} else if ( OpCodeConstants.OPTIMECODE.equals( opCode ) ) {
-			// ArtTimePacket : OpTimeCode This is an ArtTimeCode packet. It is used to transport time code over the network.
-			if ( !checkVersion( packet, hexaBrut ) )
+			}
+			return decodeArtPollPacket(packet, hexaBrut);
+		} else if (OpCodeConstants.OPTIMECODE.equals(opCode)) {
+			/*
+			 * ArtTimePacket : OpTimeCode
+			 * This is an ArtTimeCode packet.
+			 * It is used to transport time code over the network.
+			 */
+			if (!checkVersion(packet, hexaBrut)) {
 				return null;
-			return decodeArtTimeCodePacket( packet, hexaBrut );
-		} else if ( OpCodeConstants.OPPOLLREPLY.equals( opCode ) ) {
+			}
+			return decodeArtTimeCodePacket(packet, hexaBrut);
+		} else if (OpCodeConstants.OPPOLLREPLY.equals(opCode)) {
 			// ArtPollReply : This is a ArtPollReply packet.
-			return decodeArtPollReplyPacket( packet, hexaBrut );
+			return decodeArtPollReplyPacket(packet, hexaBrut);
 		}
 
 		return artNetObject;
 	}
-	
+
 	/**
 	 * Decode an artPollReplyPacket.
 	 * @param bytes is the packet data
@@ -81,32 +90,32 @@ public class ArtNetPacketDecoder {
 	 */
 	private static ArtPollReply decodeArtPollReplyPacket( byte[] bytes, String hexaBrut ) {
 		final ArtPollReply artPollReply = new ArtPollReply();
-		
+
 		// IP Adress (4*8)
-		final byte[] adress = new byte[4];
-		System.arraycopy(bytes, 10, adress, 0, 4);
+		final byte[] address = new byte[4];
+		System.arraycopy(bytes, 10, address, 0, 4);
 		try {
-			final InetAddress inetAdress = InetAddress.getByAddress( adress );
-			artPollReply.setIp( inetAdress.getHostAddress() );
+			final InetAddress inetAdress = InetAddress.getByAddress(address);
+			artPollReply.setIp(inetAdress.getHostAddress());
 		} catch (UnknownHostException e) {
-			artPollReply.setIp( null );
+			artPollReply.setIp(null);
 		}
-		
+
 		// Port (2*8)
-		artPollReply.setPort( ByteUtilsArt.byte2toIn(bytes, 14) );
-		
+		artPollReply.setPort(ByteUtilsArt.byte2toIn(bytes, 14));
+
 		// Version High (1*8)
-		artPollReply.setVersionH( bytes[16] );
-		
+		artPollReply.setVersionH(bytes[16]);
+
 		// Version Low (1*8)
-		artPollReply.setVersionL( bytes[15] );
+		artPollReply.setVersionL(bytes[15]);
 		
 		// Subnet (1*8) and subswtich (1*8)
-		artPollReply.setSubNet( String.format("%02X", bytes[18]) );
-		artPollReply.setSubSwitch( String.format("%02X", bytes[19]) );
+		artPollReply.setSubNet(String.format("%02X", bytes[18]));
+		artPollReply.setSubSwitch(String.format("%02X", bytes[19]));
 		
 		// Oem Hi (1*8) + Oem (1*8)
-		artPollReply.setOemHexa( String.format("%02X", bytes[20]) + String.format("%02X", bytes[21]) );
+		artPollReply.setOemHexa(String.format("%02X", bytes[20]) + String.format("%02X", bytes[21]));
 		
 		// UBEA Version (1*8) / 0 if not programmed
 		artPollReply.setUbeaVersion( bytes[22] );
@@ -138,84 +147,82 @@ public class ArtNetPacketDecoder {
 		}
 
 		artPollReply.setArtPollReplyStatus(artPollReplyStatus);
-		
+
 		// EstaManHi (1*8) + EstaManLow (1*8)
-		artPollReply.setEsta( new String (bytes, 24, 2 ) );
+		artPollReply.setEsta(new String (bytes, 24, 2));
 
 		// Short Name
-		artPollReply.setShortName( new String (bytes, 26, 18 ) );
-		
+		artPollReply.setShortName(new String (bytes, 26, 18));
+
 		// Long Name
-		artPollReply.setLongName( new String (bytes, 44, 64 ) );
-		
+		artPollReply.setLongName(new String (bytes, 44, 64));
+
 		return artPollReply;
 	}
-	
+
 	/**
 	 * Decode an artTimeCodePacket.
 	 * @param bytes is the packet data
 	 * @return the ArtPollPacketObject
 	 */
-	private static ArtTimeCode decodeArtTimeCodePacket( byte[] bytes, String hexaBrut ) {		
+	private static ArtTimeCode decodeArtTimeCodePacket(byte[] bytes, String hexaBrut) {
 		final ArtTimeCode artTimeCode = new ArtTimeCode();
-		artTimeCode.setFrameTime( (int) bytes[14] );
-		artTimeCode.setSeconds( (int) bytes[15] );
-		artTimeCode.setMinutes( (int) bytes[16] );
-		artTimeCode.setHours( (int) bytes[17] );
+		artTimeCode.setFrameTime((int) bytes[14]);
+		artTimeCode.setSeconds((int) bytes[15]);
+		artTimeCode.setMinutes((int) bytes[16]);
+		artTimeCode.setHours((int) bytes[17]);
 		final int typeTimecode = (int) bytes[18];
-		
-		if ( typeTimecode == 0 ) {
-			artTimeCode.setArtTimeCodeType( ArtTimeCodeType.FILM );
-		} else if ( typeTimecode == 1 ) {
-			artTimeCode.setArtTimeCodeType( ArtTimeCodeType.EBU );
-		} else if ( typeTimecode == 2 ) {
-			artTimeCode.setArtTimeCodeType( ArtTimeCodeType.DF );
-		} else if ( typeTimecode == 3 ) {
-			artTimeCode.setArtTimeCodeType( ArtTimeCodeType.SMPTE );
+
+		if (typeTimecode == 0) {
+			artTimeCode.setArtTimeCodeType(ArtTimeCodeType.FILM);
+		} else if (typeTimecode == 1) {
+			artTimeCode.setArtTimeCodeType(ArtTimeCodeType.EBU);
+		} else if (typeTimecode == 2) {
+			artTimeCode.setArtTimeCodeType(ArtTimeCodeType.DF);
+		} else if (typeTimecode == 3) {
+			artTimeCode.setArtTimeCodeType(ArtTimeCodeType.SMPTE);
 		}
-		
 		return artTimeCode;
 	}
-	
+
 	/**
 	 * Decode an artPollPacket.
-	 * @param packet is the packet data
+	 * @param bytes is the packet data
 	 * @return the ArtPollPacketObject
 	 */
 	private static ArtPoll decodeArtPollPacket( byte[] bytes, String hexaBrut ) {	
 		final ArtPoll artPoll = new ArtPoll();
-			
-		artPoll.setArtPollReplyWhenConditionsChanges( ByteUtilsArt.bitIsSet(bytes[12], 1) );
-		artPoll.setSendMeDiagnosticsMessage( ByteUtilsArt.bitIsSet(bytes[12], 2) );
 
-		if ( ByteUtilsArt.bitIsSet(bytes[12], 3) ) {
-			artPoll.setNetworkCommunicationTypeDiagnosticsMessages( NetworkCommunicationTypeEnum.UNICAST );
+		artPoll.setArtPollReplyWhenConditionsChanges(ByteUtilsArt.bitIsSet(bytes[12], 1));
+		artPoll.setSendMeDiagnosticsMessage(ByteUtilsArt.bitIsSet(bytes[12], 2));
+
+		if (ByteUtilsArt.bitIsSet(bytes[12], 3)) {
+			artPoll.setNetworkCommunicationTypeDiagnosticsMessages(NetworkCommunicationTypeEnum.UNICAST);
 		} else {
-			artPoll.setNetworkCommunicationTypeDiagnosticsMessages( NetworkCommunicationTypeEnum.BROADCAST );
+			artPoll.setNetworkCommunicationTypeDiagnosticsMessages(NetworkCommunicationTypeEnum.BROADCAST);
 		}
-		
+
 		return artPoll;
 	}
-	
+
 	/**
 	 * Check the version of artnet.
 	 * @return
 	 */
-	private static boolean checkVersion( byte[] packet, String hexaBrut ) {
+	private static boolean checkVersion(byte[] packet, String hexaBrut) {
 		final int version = (int) packet[11];
-		return ( version >= Constants.ART_NET_VERSION );
+		return (version >= Constants.ART_NET_VERSION);
 	}
 
 	/**
-	 * Convert byte to hexa.	
+	 * Convert byte to hexa.
 	 * @param barray is the byte array.
 	 * @return the String in hexa value
 	 */
-	private static String byteArrayToHex(byte[] barray) {
+	private static String byteArrayToHex(final byte[] barray) {
 	    char[] c = new char[barray.length * 2];
 	    byte b;
-	    for (int i = 0; i < barray.length; ++i)
-	    {
+	    for (int i = 0; i < barray.length; ++i) {
 	        b = ((byte)(barray[i] >> 4));
 	        c[i * 2] = (char)(b > 9 ? b + 0x37 : b + 0x30);
 	        b = ((byte)(barray[i] & 0xF));
@@ -224,5 +231,4 @@ public class ArtNetPacketDecoder {
 
 	    return new String(c);
 	}
-	
 }
