d9bf567785b22712a5f8bdedcdc172ba27771a74
Matthias Vill <github@cconstruct.de>
2012-08-06 23:04:24 +0200

    Corrected: - pom.xml to include used encoding (or at least a similar encoding)  - pom.xml to use a Java-Version with enums (and not the 1.3 default)  - ByteUtilsArt#byte2toIn to correctly handle bytes where the 7-bit is set, as Java does not know unsigned byte-values  Add:  - JUnit 4.5 testing framework  - A test to show why my change to ByteUtilsArt#byte2toIn is necessary

---
+++ src/test/java/fr/azelart/artnetstack/utils/ByteUtilsArtTest.java
@@ -0,0 +1,32 @@
+package fr.azelart.artnetstack.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: matthias
+ * Date: 06.08.12
+ * Time: 22:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class ByteUtilsArtTest {
+    @Test
+    public void testByte2toIn() throws Exception {
+        byte[][] test_data = new byte[][]{
+                new byte[]{0x00, 0x00},
+                new byte[]{(byte) 0xff, 0x00},
+                new byte[]{0x00, (byte) 0xff},
+                new byte[]{(byte) 0xff, (byte) 0xff}};
+        int[] results = new int[]{
+                0x0000,
+                0x00ff,
+                0xff00,
+                0xffff};
+        for (int i = 0; i < test_data.length; i++) {
+            Assert.assertEquals(Arrays.toString(test_data[i]) + " should be converted to " + results[i], results[i], ByteUtilsArt.byte2toIn(test_data[i], 0));
+        }
+    }
+}
